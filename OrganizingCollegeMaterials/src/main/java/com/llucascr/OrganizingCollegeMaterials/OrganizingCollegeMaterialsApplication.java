242af878e0a796a4078bb760102eb15a3fc8ca88
llucascr <lucasdecamposranzani@gmail.com>
2025-01-19 15:37:59 -0300

    Corpo do projeto / Configuração e instalação de dependencias

---
+++ OrganizingCollegeMaterials/src/main/java/com/llucascr/OrganizingCollegeMaterials/OrganizingCollegeMaterialsApplication.java
@@ -0,0 +1,13 @@
+package com.llucascr.OrganizingCollegeMaterials;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrganizingCollegeMaterialsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrganizingCollegeMaterialsApplication.class, args);
+	}
+
+}
---
+++ OrganizingCollegeMaterials/src/test/java/com/llucascr/OrganizingCollegeMaterials/OrganizingCollegeMaterialsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.llucascr.OrganizingCollegeMaterials;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrganizingCollegeMaterialsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
