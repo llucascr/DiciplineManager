817b2264d2730e9de25cfb7cd58c94d435c75d30
llucascr <lucasdecamposranzani@gmail.com>
2025-01-21 17:52:22 -0300

    img modelo de objetos

--- OrganizingCollegeDisciplines/src/main/resources/import.sql
+++ OrganizingCollegeDisciplines/src/main/resources/import.sql
@@ -1,9 +1,5 @@
 INSERT INTO tb_student (birthday_date, email, name) VALUES ('2000-05-15', 'john.doe@example.com', 'John Doe');
-
 INSERT INTO tb_student (birthday_date, email, name) VALUES ('1999-12-03', 'jane.smith@example.com', 'Jane Smith');
-
 INSERT INTO tb_student (birthday_date, email, name) VALUES ('2001-07-20', 'emily.jones@example.com', 'Emily Jones');
-
 INSERT INTO tb_student (birthday_date, email, name) VALUES ('1998-09-10', 'michael.brown@example.com', 'Michael Brown');
-
 INSERT INTO tb_student (birthday_date, email, name) VALUES ('2002-01-25', 'sarah.davis@example.com', 'Sarah Davis');
